b9e9844ec5f1faecec5debca6eda0b894cbff125
alecsmith84 <astonmartindbrs9>
2019-05-13 22:51:30 -0500

    done

--- parseJSon/ViewController.swift
+++ parseJSon/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
+struct Country: Decodable {
+    let name: String
+    let capital: String
+    let region: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = "https://api.myjson.com/bins/x45nm"
+        
+        let urlObj = URL(string:url)
+        
+        URLSession.shared.dataTask(with: urlObj!) {
+            (data, response, error) in
+            
+            do {
+                let countries = try JSONDecoder().decode([Country].self, from: data!)
+                
+                for country in countries {
+                    print(country.name + ": " + country.capital)
+                }
+            } catch {
+                print("we got an error")
+            }
+        }.resume()
+        
     }
 
 
