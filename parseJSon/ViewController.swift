f2d06091fc92145de9e43e339faafc1eaf7ed995
alecsmith84 <astonmartindbrs9>
2019-05-13 22:42:31 -0500

    Initial Commit

---
+++ parseJSon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parseJSon
+//
+//  Created by Alec Smith on 5/13/19.
+//  Copyright © 2019 Alec Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
